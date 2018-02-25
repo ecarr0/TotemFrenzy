b45ce04f9bf01f90bc8ab16d0b4ee2f99e81955d
ecarr0 <ekar@ucvts.tec.nj.us>
2018-02-25 17:51:38 -0500

    Add files via upload

---
+++ AudioScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class AudioScript : MonoBehaviour {
+
+	static bool AudioBegin = false;
+    new AudioSource audio;
+    void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+        if (!AudioBegin)
+        {
+            audio.Play();
+            DontDestroyOnLoad(gameObject);
+            AudioBegin = true;
+        }
+    }
+    void Update()
+    {
+        string name = SceneManager.GetActiveScene().name;
+        if (name == "outside")
+        {
+            AudioBegin = false;
+            Destroy(this.gameObject);
+        }
+    }
+}
